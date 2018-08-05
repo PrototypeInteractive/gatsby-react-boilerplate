ef788c2fe1a8eb6cecceea11db0037442fe95598
James Nolan <j.nolan@otherwise.ch>
2018-08-05 13:56:03 +0100

    Remove unused button component

--- src/components/button/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import './_button.scss';
-
-const Button = props => (
-  <a href={props.link} className={`button ${props.className}`}>{props.text}</a>
-);
-
-export default Button;
