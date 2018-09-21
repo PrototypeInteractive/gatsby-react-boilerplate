afd1697337382c8d9916bbadbc585d567de6706c
khodor <khodor.ammar@gmail.com>
2018-09-21 18:23:03 +0400

    upgrade to gatsby@2

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,6 @@ module.exports = {
     title: `Gatsby Default Starter`,
   },
   plugins: [
-    `gatsby-plugin-react-next`,
     `gatsby-plugin-react-helmet`,
     `svgo`,
     `gatsby-plugin-sass`,
