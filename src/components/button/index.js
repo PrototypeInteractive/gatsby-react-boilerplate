015f7c51a35ad21afd2c6751c3ba610e3d4b8ca9
khodor <khodor.ammar@gmail.com>
2018-11-01 11:51:14 +0400

    fix include of buttons styles

--- src/components/button/index.js
+++ src/components/button/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-import "./button.scss";
+import "./_button.scss";
 
 const Button = props => (
   <a href={props.link} className={`button ${props.className}`}>
