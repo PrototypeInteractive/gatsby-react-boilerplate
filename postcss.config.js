cf98a552120d2f74035c37c53b29eef49521679b
khodor <khodor.ammar@gmail.com>
2019-01-11 19:19:59 +0400

    update dependecies and remove pixrem

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,7 @@
-const pixrem = require("pixrem");
 const autoprefixer = require("autoprefixer");
 
 module.exports = () => ({
   plugins: [
-    pixrem(),
     autoprefixer({
       browsers: ["last 2 versions"]
     })
